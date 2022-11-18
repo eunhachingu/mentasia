79092415d06c5bb0a014812f4ee05002bc57f803
eunhachingu <espajunarjr@gmail.com>
2022-11-19 00:18:04 +0800

    initial homescreen

--- lib/src/features/authentication/controllers/splash_screen_controller.dart
+++ lib/src/features/authentication/controllers/splash_screen_controller.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../screens/apphome.dart';
+import '../screens/welcome-screen/homescreen.dart';
 
 class SplashScreenController extends GetxController {
   static SplashScreenController get find => Get.find();
@@ -13,7 +13,7 @@ class SplashScreenController extends GetxController {
     animate.value = true;
     await Future.delayed(Duration(milliseconds: 3000));
     // ignore: use_build_context_synchronously
-    Get.to(const AppHome());
+    Get.to(const HomeScreen());
     // Navigator.pushReplacement(
     //     context, MaterialPageRoute(builder: (context) => const AppHome()));
   }
