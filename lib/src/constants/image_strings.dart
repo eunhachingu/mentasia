0661b356c07cc59b2cfef9b4c8ead0351748b73f
eunhachingu <espajunarjr@gmail.com>
2022-11-19 14:12:14 +0800

    initial support settings

--- lib/src/constants/image_strings.dart
+++ lib/src/constants/image_strings.dart
@@ -12,3 +12,10 @@ const String tNotification = "assets/logo/notification-logo.png";
 const String tPersonalInfo = "assets/logo/personal-info-logo.png";
 const String tTermsandService = "assets/logo/terms-of-service-logo.png";
 const String tCancel = "assets/logo/cancel-logo.png";
+
+// Support Settings
+
+const String tNumber1 = "assets/logo/number1-logo.png";
+const String tNumber2 = "assets/logo/number2-logo.png";
+const String tNumber3 = "assets/logo/number3-logo.png";
+
--- lib/src/features/authentication/controllers/splash_screen_controller.dart
+++ lib/src/features/authentication/controllers/splash_screen_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:mentasia/src/features/authentication/screens/settings_screen.dart';
+import 'package:mentasia/src/features/authentication/screens/splash_screen/settings/settings_screen.dart';
+import 'package:mentasia/src/features/authentication/screens/splash_screen/settings/support/mentasia_works_screen.dart';
 
 import '../screens/homescreen.dart';
 
@@ -14,7 +15,7 @@ class SplashScreenController extends GetxController {
     animate.value = true;
     await Future.delayed(Duration(milliseconds: 3000));
     // ignore: use_build_context_synchronously
-    Get.to(const SettingsScreen());
+    Get.to(const MentasiaWorksScreen());
     // Navigator.pushReplacement(
     //     context, MaterialPageRoute(builder: (context) => const AppHome()));
   }
