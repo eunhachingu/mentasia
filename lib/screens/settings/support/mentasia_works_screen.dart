4172d55bc10b804820bdcf3818ceee27dd3dd2b5
eunhachingu <espajunarjr@gmail.com>
2022-12-05 23:28:42 +0800

    revised mentasia works

--- lib/screens/settings/support/mentasia_works_screen.dart
+++ lib/screens/settings/support/mentasia_works_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mentasia/utils/mentasia_card.dart';
 
 import '../../../constants/image_strings.dart';
 
@@ -14,160 +15,44 @@ class _MentasiaWorksScreenState extends State<MentasiaWorksScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
+      body: Stack(
+        // ignore: prefer_const_literals_to_create_immutables
         children: [
-          // How Mentasia Works
-          const Center(
+          Align(
+            alignment: Alignment.topCenter,
             child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 40),
+              padding: EdgeInsets.symmetric(vertical: 20.0),
               child: Text(
                 "How Mentasia Works",
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                ),
+                style: Theme.of(context).textTheme.labelLarge,
               ),
             ),
           ),
-
-          // Sign in
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                child: const Image(
-                  image: AssetImage(tNumber1),
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),
-                  color: Colors.blue,
-                ),
-                height: 136,
-                width: 350,
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    // ignore: prefer_const_literals_to_create_immutables
-                    children: [
-                      const Text(
-                        "Sign-in",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const Text(
-                        textAlign: TextAlign.justify,
-                        "Sign-in using Google or Facebook to save your progress and information in our application. It can help us optimize to make your experience personalized.",
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
+          const Positioned(
+            top: 150,
+            right: 50,
+            child: MentasiaCard(
+              image: tGmail,
+              labelText:
+                  "Sign in using Google or Facebook to optimized your personal experienced",
+            ),
           ),
-
-          SizedBox(
-            height: 20,
+          const Positioned(
+            top: 350,
+            left: 50,
+            child: MentasiaCard(
+              image: tChat,
+              labelText:
+                  "Chat with our chatbot, the bot will provide your needs.",
+            ),
           ),
-
-          // Chat
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                child: const Image(
-                  image: AssetImage(tNumber2),
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),
-                  color: Colors.blue,
-                ),
-                height: 136,
-                width: 350,
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    // ignore: prefer_const_literals_to_create_immutables
-                    children: [
-                      const Text(
-                        "Chat",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const Text(
-                        textAlign: TextAlign.justify,
-                        "After signing-in, you can now proceed to have a chat with our chatbot. Our bot will help you find  as well as provide your needs.",
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-
-          SizedBox(
-            height: 20,
-          ),
-
-          // Enjoy
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                child: const Image(
-                  image: AssetImage(tNumber3),
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),
-                  color: Colors.blue,
-                ),
-                height: 136,
-                width: 350,
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    // ignore: prefer_const_literals_to_create_immutables
-                    children: [
-                      const Text(
-                        "Enjoy",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const Text(
-                        textAlign: TextAlign.justify,
-                        "The chatbot can provide you two things, a compassionate interlocutor and a contact to a professional.",
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
+          const Positioned(
+            top: 550,
+            right: 50,
+            child: MentasiaCard(
+              image: tMind,
+              labelText: "Compassionate Interlocutor, Contact to professional.",
+            ),
           ),
         ],
       ),
---
+++ lib/utils/mentasia_card.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../constants/image_strings.dart';
+
+class MentasiaCard extends StatelessWidget {
+  final String image;
+  final String labelText;
+
+  const MentasiaCard({super.key, required this.image, required this.labelText});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Container(
+        padding: EdgeInsets.all(5),
+        color: Colors.grey[300],
+        width: 150,
+        child: Column(
+          children: [
+            Image(
+              image: AssetImage(image),
+            ),
+            Center(
+              child: Text(labelText),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
